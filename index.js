6e5c212f5d35b66a1f171a0b00b4c67afa139064
Dev-Hood <estudartagc@gmail.com>
2021-09-12 21:03:55 -0300

    tela principal(com alterações a serem feitas)

---
+++ index.js
@@ -0,0 +1,15 @@
+const forma = document.getElementsByClassName('fluid')
+const container = document.querySelector('.tela-inicio')
+
+container.addEventListener('mousemove',(e)=>{
+    let eixoX = (window.innerWidth/2-e.pageX)/50
+    forma[0].style.transform = `translate(100px, 100px) skew(${eixoX}deg) scale(1.5)`;
+    forma[1].style.transform = `translate(100px, 100px) skew(${eixoX}deg) `;
+    forma[2].style.transform = `rotate(${-eixoX*3}deg)`
+    forma[3].style.transform = `rotate(${-eixoX*8}deg)`
+    forma[4].style.transform = `rotate(${-eixoX*4}deg)`
+})
+container.addEventListener('mouseleave',()=>{
+    forma[0].style.transform = `translate(100px, 100px) skew(${0}deg) scale(1.5)`;
+    forma[1].style.transform = `translate(100px, 100px) skew(${0}deg) `;
+})
